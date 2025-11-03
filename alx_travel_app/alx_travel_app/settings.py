f07b55a8f58dc6b58e4baf396ebbb3fd4890f154
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-03 05:25:11 +0200

    Update settings.py

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -1,1 +1,25 @@
+import environ
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+app = Celery('alx_travel_app')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+env = environ.Env()
+environ.Env.read_env()  # reads .env file
+
+SECRET_KEY = env('SECRET_KEY', default='dummy_secret_key')
+DEBUG = env.bool('DEBUG', default=True)
+...
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
 
