44be2c5029a398150bde0c09c0803e74e1b3c471
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-03 06:06:31 +0200

    Update urls.py

---
+++ alx_travel_app/alx_travel_app/listings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'listings'
+
+urlpatterns = [
+    path('', views.property_list, name='property_list'),
+    path('property/<int:pk>/', views.property_detail, name='property_detail'),
+    path('property/<int:property_id>/book/', views.booking_create, name='booking_create'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
+    path('property/<int:property_id>/review/', views.review_create, name='review_create'),
+]
