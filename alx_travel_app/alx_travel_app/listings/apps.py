b54c7340addd6c73c8ea83b5bc80d035f1c5c2e7
Matthew Ballah Kollie, III. <mballahkollie2@gmail.com>
2025-11-03 06:07:07 +0200

    Update apps.py

--- alx_travel_app/alx_travel_app/listings/apps.py
+++ alx_travel_app/alx_travel_app/listings/apps.py
@@ -1,6 +1,7 @@
-from django.apps import AppConfig  # type: ignore
+from django.apps import AppConfig
 
 
 class ListingsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'listings'
+    verbose_name = 'Travel Listings'
